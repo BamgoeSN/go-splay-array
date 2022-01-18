1eeb9802ee5b5d28bdde87221b56deff29ca5375
unknown <60645387+BamgoeSN@users.noreply.github.com>
2022-01-18 15:27:49 +0900

    Implement CopyToSlice

--- splay.go
+++ splay.go
@@ -10,3 +10,24 @@ func (s *Splay) Len() int {
 	}
 	return s.root.cnt
 }
+
+// CopyToSlice returns a slice converted from the splay array. The slice returned is not synced with the splay array;
+// i.e. even if the splay array is updated later on, the returned slice remains the same.
+func (s *Splay) CopyToSlice() []interface{} {
+	if s.root == nil {
+		return nil
+	}
+	arr := make([]interface{}, 0, s.root.cnt)
+	s.toSliceHelper(&arr, s.root)
+	return arr
+}
+
+func (s *Splay) toSliceHelper(arr *[]interface{}, curr *node) {
+	if curr.l != nil {
+		s.toSliceHelper(arr, curr.l)
+	}
+	*arr = append(*arr, curr.data)
+	if curr.r != nil {
+		s.toSliceHelper(arr, curr.r)
+	}
+}
